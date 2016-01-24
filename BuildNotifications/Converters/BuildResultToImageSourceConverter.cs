90d0da702c07487c7b29fb82e9eac33c9e85ee35
Nicola Crawford <nicola.crawford@intergen.co.nz>
2016-01-25 12:37:48 +1300

    Missed this file

---
+++ BuildNotifications/Converters/BuildResultToImageSourceConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using BuildNotifications.Model;
+
+namespace BuildNotifications.Converters
+{
+    public class BuildResultToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            BuildResult? result = value == null ? null : (BuildResult?)Enum.Parse(typeof (BuildResult), value.ToString());
+            switch (result)
+            {
+                case BuildResult.Succeeded:
+                    return "Icons/tickResource.ico";
+                case BuildResult.PartiallySucceeded:
+                    return "Icons/warningResource.ico";
+                case BuildResult.Failed:
+                    return "Icons/crossResource.ico";
+            }
+            return "Icons/quesionResource.ico";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
